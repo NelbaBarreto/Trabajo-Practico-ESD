436159309bc21ed06a715f1754259ed9f385eea7
Nelba Barreto <barretonelba@gmail.com>
2019-10-22 19:27:34 -0300

    Avance

--- src/model/Direccion.h
+++ src/model/Direccion.h
@@ -1,17 +1,19 @@
-#include<string>
+#include <string>
+using namespace std;
 
-class Direccion{
-  private:
-    string ciudad;
-    string calle;
-    string numero;
-    string epn;
-    string pais;
-    string region;
-  public:
-    Direccion();
-    ~Direccion();
+class Direccion {
+ private:
+  string ciudad;
+  string calle;
+  string numero;
+  string epn;
+  string pais;
+  string region;
+
+ public:
+  Direccion();
+  ~Direccion();
 };
 
-Direccion::Direccion(){}
-Direccion::~Direccion(){}
+Direccion::Direccion() {}
+Direccion::~Direccion() {}
---
+++ src/model/Telefono.h
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class Telefono {
+ private:
+  string tipo;
+  string numero;
+  int interno;
+
+ public:
+  Telefono(/* args */);
+  ~Telefono();
+};
+
+Telefono::Telefono(/* args */) {}
+
+Telefono::~Telefono() {}
