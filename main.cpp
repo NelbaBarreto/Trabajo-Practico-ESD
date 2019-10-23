6eb0d9f7ea5b480010499d106358131b16eccb0c
Nelba Barreto <barretonelba@gmail.com>
2019-10-22 21:39:44 -0300

    Agregar Regex

--- main.cpp
+++ main.cpp
@@ -1,21 +1,147 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <regex>
 using namespace std;
+
+class Nodo {
+ public:
+  Nodo();
+  ~Nodo(){};
+  char data;
+  Nodo *next;
+};
+
+Nodo::Nodo() {
+  data = ' ';
+  next = NULL;
+}
+
+class Pila {
+ public:
+  Pila() { top = NULL; }
+  ~Pila();
+  Nodo *top;
+  void push(char);
+  char pop();
+  char getTop();
+  bool isEmpty();
+  int isBalanced(string);
+};
+
+Pila::~Pila() {
+  Nodo *path = top;
+  Nodo *del;
+  while (path != NULL) {
+    del = path;
+    path = path->next;
+    delete del;
+  }
+}
+
+void Pila::push(char e) {
+  Nodo *nuevo = new Nodo;
+  nuevo->data = e;
+  nuevo->next = NULL;
+  if (top == NULL) {
+    top = nuevo;
+  } else {
+    nuevo->next = top;
+    top = nuevo;
+  }
+}
+
+char Pila::pop() {
+  char temp = getTop();
+  Nodo *p = top->next;
+  if (top != NULL) {
+    delete top;
+    top = p;
+  }
+
+  return temp;
+}
+
+bool Pila::isEmpty() {
+  if (top != NULL) {
+    return false;
+  }
+
+  return true;
+}
+
+char Pila::getTop() {
+  if (top != NULL) {
+    return top->data;
+  }
+  return ' ';
+}
+
+// int Pila::isBalanced(string x) {
+//   int i = 0;
+//    regex r("<\\s+>");
+//     for(sregex_iterator i =sregex_iterator(s.begin(), s.end(), r);
+//                             i !=sregex_iterator();
+//                             ++i )
+//     {
+//        smatch m = *i;
+//        cout << m.str() << " at position " << m.position() << '\n';
+//     }
+
+//   char caracter = ' ';
+//   for (i = 0; i < x.size(); i++) {
+//     caracter = x[i];
+//     if (/*etiqueta de apertura*/) {
+//       push(caracter);
+//     }
+
+//     if /*es la x para cerrar*/ {
+//       if (!isEmpty() && getTop() == /*la etiqueta actual*/) {
+//         pop();
+//       } else {
+//         return 0;
+//       }
+//     }
+//   }
+
+//   if(isEmpty()) return 1;
+// }
 
 int main() {
   string Agenda;
   ifstream archivo;
 
   archivo.open("res/agenda.xml");
-  if(!archivo) {
-    cout << "Error";
+  if (!archivo) {
+    cout << "Error, no se pudo abrir el archivo.";
     exit(1);
   }
 
-  while(archivo >> Agenda) {
-    cout << Agenda;
+  // while (archivo >> Agenda) {
+  //   cout << Agenda;
+  // }
+
+  cout << endl
+       << endl
+       << endl
+       << endl
+       << " HERE" << endl
+       << endl
+       << endl
+       << endl;
+  string pepe;
+  while (getline(archivo, Agenda)) {
+    regex r("<[a-zA-Z_]*?>");
+  
+    for (sregex_iterator i = sregex_iterator(Agenda.begin(), Agenda.end(), r);
+         i != sregex_iterator(); ++i) {
+      smatch m = *i;
+      pepe = m.str();
+    }
+      cout << pepe;
   }
 
+
   archivo.close();
+
   return 0;
 }
